3cbc24a4192415f4d0fc680b5afcee94afb1e88c
jennijennina <starstar135@posteo.de>
2022-12-21 18:25:13 +0100

    lev1_2_js-reactjs_state-faq

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [activeClass, setActiveClass] = useState("hidden");
+
+  const toggleClass = () => {
+    setActiveClass(activeClass === "hidden" ? "show" : "hidden");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article>
+      <div>
+        <p>Why is React great?</p>
+        <button onClick={toggleClass}>
+          {activeClass === "hidden" ? "+" : "-"}
+        </button>
+      </div>
+      <div className={activeClass}>Fast Learning Curve</div>
+    </article>
   );
 }
 
